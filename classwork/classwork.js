38a06e627db077ac1d6b764ab30a76f675dca48a
hollm164 <marielleholland@marielles-mbp.wireless.newschool.edu>
2016-02-02 18:20:16 -0500

    work in progress…

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -4,6 +4,7 @@
 
 var cellSize = 50;
 
+
 function setup(){
   createCanvas(windowWidth,windowHeight);
 
@@ -11,7 +12,23 @@ function setup(){
 
     for(var column =0; column <20;column++){
 
+      fill(0,0,row*25);
+
+      var xPos =  column*cellSize;
+      var yPos = row*cellSize;
+
       rect(cellSize*column, cellSize*row, cellSize, cellSize);
+
+      stroke("white");
+      line(xPos,yPos,xPos+25, yPos+25);
+      line(xPos, yPos+cellSize, xPos+cellSize, yPos);
+      line(xPos+25,yPos,xPos+25, yPos+25);
+      line(xPos,yPos+25,xPos+25, yPos+25);
+
+      fill("aliceblue");
+      arc(xPos+25,yPos+25,cellSize, cellSize,0, HALF_PI);
+
+
 
     }
   }
