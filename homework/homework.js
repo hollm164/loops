f451dec9203180a0c413458223c34dda179fa215
hollm164 <marielleholland@Marielles-MacBook-Pro.local>
2016-02-09 11:42:54 -0500

    I had so much trouble trying to figure out how to do this. I tried to just make a simple line drawing of 100 lines that intersected and changed location and color every time the page refreshed but I couldn’t figure out why it wasn’t working for the life of me.

--- homework/homework.js
+++ homework/homework.js
@@ -3,7 +3,20 @@
 */
 
 function setup(){
-  createCanvas(windowWidth,windowHeight);
-  
-  ellipse(windowWidth/2,windowHeight/2,100,100);
+  createCanvas(600,600);
+
+  for(var i=0; i<100; i=i++){
+
+    var xPos = random(50,100);
+    var yPos = random(50,100);
+
+    var randomGreen = random(255);
+    var randomBlue = random(255);
+
+    stroke(0, random(255),random(255));
+    strokeWeight(10);
+    line(xPos,yPos,xPos+i,yPos+i);
+
+
+    }
 }
